a420624c555c0133650fb3d03f4e89889eb2b952
CX <2260330761@qq.com>
2022-09-19 18:24:02 +0800

    20220919

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -5,5 +5,6 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
     (nemu_state.state == NEMU_QUIT);
+  Log("\nnemu_state = %d\ngood = %d/n!good = %d\n",nemu_state.state,good,!good);
   return !good;
 }
